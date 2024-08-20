0260027864ca6bd06541aaf458849c95044009fd
shivPosted <shivpratap621@gmail.com>
2024-08-20 19:58:39 +0530

    using prop types to make star component more reusable

--- src/StarComponent.jsx
+++ src/StarComponent.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 const styleContainer = {
   display: 'flex',
   gap: '16px',
   alignItems: 'center',
-  backgroundColor: '#2A3335',
-  padding: '16px 12px',
-  maxWidth: '20%',
-  borderRadius: '9px',
-  color: '#fff',
-  justifyContent: 'center',
+  // backgroundColor: '#2A3335',
+  // padding: '16px 12px',
+  // maxWidth: '20%',
+  // borderRadius: '9px',
+  // color: '#fff',
+  // justifyContent: 'center',
 };
 
 const styleStarContainer = {
@@ -29,16 +30,30 @@ const styleRatingNum = {
   fontFamily: 'sans-serif',
 };
 
-const styleStar = {
-  width: '24px',
-  height: '24px',
-  cursor: 'pointer',
+StarComponent.propTypes = {
+  maxLength: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  ratingString: PropTypes.array,
+  onSetRating: PropTypes.func,
+  defaultRating: PropTypes.number,
 };
 
-export default function StarComponent({ maxLength = 5 }) {
-  const [starCount, setStarCount] = useState(0);
+export default function StarComponent({
+  maxLength = 5,
+  size = 24,
+  color = '#FFD700',
+  ratingString = [],
+  onSetRating = () => {},
+  defaultRating = 0,
+}) {
+  const [starCount, setStarCount] = useState(defaultRating);
   const [tempStarCount, setTempStarCount] = useState(0);
 
+  function handleClick(value) {
+    setStarCount(cur => (cur === value ? 0 : value));
+    onSetRating(value);
+  }
   return (
     <div className="container" style={styleContainer}>
       <div
@@ -49,15 +64,15 @@ export default function StarComponent({ maxLength = 5 }) {
         {Array.from({ length: maxLength }, (_, i) => {
           return (
             <Star
+              color={color}
+              size={size}
               full={tempStarCount ? tempStarCount >= i + 1 : starCount >= i + 1}
               key={i + 1}
               handleMouseEnter={() => {
                 setTempStarCount(i + 1);
               }}
               // handleMouseLeave={() => setTempStarCount(0)}
-              handleClick={() =>
-                setStarCount(cur => (cur === i + 1 ? 0 : i + 1))
-              }
+              handleClick={() => handleClick(i + 1)}
             />
           );
         })}
@@ -71,22 +86,29 @@ export default function StarComponent({ maxLength = 5 }) {
           height: '16px',
         }}
       >
-        {tempStarCount || starCount}
+        {ratingString.length === 0
+          ? tempStarCount || starCount
+          : ratingString[tempStarCount ? tempStarCount - 1 : starCount - 1]}
       </p>
     </div>
   );
 }
 
-function Star({ handleClick, full, handleMouseEnter }) {
+function Star({ handleClick, full, handleMouseEnter, size, color }) {
+  const styleStar = {
+    width: `${size}px`,
+    height: `${size}px`,
+    cursor: 'pointer',
+  };
   return (
     <svg
       onClick={handleClick}
       onMouseEnter={handleMouseEnter}
       // onMouseLeave={handleMouseLeave}
       xmlns="http://www.w3.org/2000/svg"
-      fill={full ? '#FFD700' : 'none'}
+      fill={full ? color : 'none'}
       viewBox="0 0 24 24"
-      stroke="#FFD700"
+      stroke={color}
       style={styleStar}
     >
       <path
@@ -96,6 +118,16 @@ function Star({ handleClick, full, handleMouseEnter }) {
         d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
       />
     </svg>
+  );
+}
+
+export function Test() {
+  const [displayRating, setDisplayRating] = useState(0);
+  return (
+    <div className="container">
+      <StarComponent onSetRating={setDisplayRating} />
+      <p>This product has been rated {displayRating} stars</p>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react';
+import { StrictMode, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 // import App from './App.jsx';
+import { Test } from './StarComponent';
 import StarComponent from './StarComponent';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
     <StarComponent maxLength={10} />
+    <StarComponent
+      maxLength={5}
+      color="red"
+      size={24}
+      ratingString={['Terrible', 'Bad', 'Ok', 'Good', 'Amazing']}
+    />
+    <StarComponent defaultRating={3} color="blue" />
+    <Test />
   </StrictMode>
 );
