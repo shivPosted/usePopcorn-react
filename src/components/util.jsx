c58457deb3956a4113a8262fb8760c57f2090b0f
shivPosted <shivpratap621@gmail.com>
2025-07-28 21:21:27 +0530

    fixed api key issue

--- src/components/util.jsx
+++ src/components/util.jsx
@@ -1,6 +1,6 @@
 import supabase from "../supabse";
 
-export const API_key = import.meta.env.VITE_API_key;
+export const API_key = import.meta.env.VITE_OMDB_API_KEY;
 
 export const average = (arr) =>
   arr.reduce((accum, cur) => accum + cur) / arr.length;
